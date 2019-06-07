852d578d1a6c5004cbcc10b6e57a1cdb1bbda96e
majuz96 <47798374+majuz96@users.noreply.github.com>
2019-06-06 19:46:18 -0600

    Add files via upload

---
+++ js/my-app.js
@@ -0,0 +1,47 @@
+// Initialize your app
+var myApp = new Framework7({
+	root:'#app',
+	theme: 'md', //ios, md, auto
+    modalTitle: 'Framework7', 	//title for modals
+	panel: {
+    	swipe: 'left',
+  	},
+	cache:false,
+	pushState:false,
+	swipeBackPage:true,
+	material: true,
+	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		
+		{
+		path: '/list/',
+    	url: 'list.html',
+    	name: 'list',
+  		},
+		
+		{
+		path: '/cards/',
+    	url: 'cards.html',
+    	name: 'cards',
+  		},
+		{
+		path: '/medicinageneral/',
+    	url: 'medicinageneral.html',
+    	name: 'medicinageneral',
+  		}
+	]
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+
+
+
+
+ 
+
